81bba0db903758ae56d878b8ac1a238007513d72
Malachi Gruenhagen <68450431+nurse-the-code@users.noreply.github.com>
2023-02-21 23:40:41 -0500

    new upstream repo

--- src/Babylon/EngineSystem.ts
+++
@@ -1,8 +0,0 @@
-import { Engine, Scene, UniversalCamera } from "babylonjs";
-
-export const BabylonSystem = {
-  scene: <Scene>{},
-  engine: <Engine>{},
-  camera: <UniversalCamera>{},
-  canvas: <HTMLCanvasElement>{},
-};
--- src/contexts/Nexus/nexus.ts
+++
@@ -1,8 +0,0 @@
-import { DVEN } from "divine-voxel-engine/dist/Nexus/DivineVoxelEngineNexus";
-import { DVP } from "divine-voxel-engine/dist/Plugins/Physics/Nexus/DivineVoxelPhysics";
-import { GetNexusPlayer } from "./Player/NexusPlayer";
-
-(async () => {
-  await DVEN.$INIT();
-  const player = await GetNexusPlayer(DVEN, DVP);
-})();
